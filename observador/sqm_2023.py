3b64230413263e8ab3dc34dd52847469a83352eb
Alvaro Diaz Valenzuela <alvaro@efaa.cl>
2025-03-11 08:47:44 -0300

    Cada gráfico es una función distinta. Se modifica el orden en que aprecen los gráficos en la hoja Dashboard.

--- observador/sqm_2023.py
+++ observador/sqm_2023.py
@@ -3,52 +3,16 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 
-col1, col2 = st.columns(2, vertical_alignment="bottom", border=True)
 
-pais = pd.read_excel("data/sqm.xlsx", sheet_name="PAIS")
-with col1:
+def vee():
+    pais = pd.read_excel("data/sqm.xlsx", sheet_name="PAIS")
     agno = st.selectbox("Selecciona el año", options=["2022", "2023"])
-    total = pais[f"A{agno}"].sum()
-    fig = px.bar(pais, x="Pagos", y=f"A{agno}", title=f"VEE (en Chile) - USD {total:,.0f} millones")
+    total = pais[f"Año {agno}"].sum()
+    fig = px.bar(pais, x="Pagos", y=f"Año {agno}", title=f"VEE (en Chile) - USD {total:,.0f} millones")
     st.plotly_chart(fig)
 
-no = pd.read_excel("data/sqm.xlsx", sheet_name="NO")
-no_ok = no[(no.Glosa == "Renta") | (no.Glosa == "IVA")]
-fig = px.bar(
-    no_ok,
-    x=["Año"],
-    y="Valor",
-    color="Glosa",
-    title=f"Pagos al Estado (millones de USD"
-)
-with col2:
-    st.plotly_chart(fig)
 
-col3, col4 = st.columns(2, border=True)
-with col3:
-    no2 = pd.read_excel("data/sqm.xlsx", sheet_name="NO2")
-    fig = px.bar(
-        no2,
-        x=["Año"],
-        y="Valor",
-        color="Glosa",
-        title="Impulso al Empleo (2019 - 2023)"
-    )
-    st.plotly_chart(fig)
-
-with col4:
-    no2a = pd.read_excel("data/sqm.xlsx", sheet_name="NO2a")
-    fig = px.bar(
-        no2a,
-        x="Glosa",
-        y="Valor",
-        color="Región",
-        title="Fomento al Empleo Local Año 2023 (Nº Personas)"
-    )
-    st.plotly_chart(fig)
-
-col5, col6 = st.columns(2, border=True, vertical_alignment="bottom")
-with col5:
+def impacto_economico():
     agno2 = st.selectbox("Selecciona el año", key="agno2", options=["2022", "2023"])
     paisa = pd.read_excel("data/sqm.xlsx", sheet_name="PAISa")
     impacto = paisa.loc[paisa["Glosa"] == "IMPACTO ECON", f"A{agno2}"].iloc[0]
@@ -68,7 +32,45 @@ with col5:
 
     st.plotly_chart(fig)
 
-with col6:
+
+def pagos_estado():
+    no = pd.read_excel("data/sqm.xlsx", sheet_name="NO")
+    no_ok = no[(no.Glosa == "Renta") | (no.Glosa == "IVA")]
+    fig = px.bar(
+        no_ok,
+        x=["Año"],
+        y="Valor",
+        color="Glosa",
+        title=f"Pagos al Estado (millones de USD"
+    )
+    st.plotly_chart(fig)
+
+
+def impulso_empleo():
+    no2 = pd.read_excel("data/sqm.xlsx", sheet_name="NO2")
+    fig = px.bar(
+        no2,
+        x=["Año"],
+        y="Valor",
+        color="Glosa",
+        title="Impulso al Empleo (2019 - 2023)"
+    )
+    st.plotly_chart(fig)
+
+
+def empleo_local_importado():
+    no2a = pd.read_excel("data/sqm.xlsx", sheet_name="NO2a")
+    fig = px.bar(
+        no2a,
+        x="Glosa",
+        y="Valor",
+        color="Región",
+        title="Empleo Local vs Importado - Año 2023 (Nº Personas)"
+    )
+    st.plotly_chart(fig)
+
+
+def valor_irradiado():
     agno3 = st.selectbox("Selecciona el año", key="agno3", options=[2022, 2023])
     paisb = pd.read_excel("data/sqm.xlsx", sheet_name="PAISb")
     paisb_ok = paisb[paisb.Agno == agno3].copy()
@@ -76,7 +78,30 @@ with col6:
     fig = px.treemap(paisb_ok, path=[px.Constant("Todos"), 'Agno', 'Glosa'], values='Valor', hover_data="Valor")
     fig.update_traces(root_color="lightgrey")
     fig.update_layout(
-        title=f"Valor Irradiado {agno3}: d .{total:,.0f} millones USD",
+        title=f"Valor Irradiado {agno3}: {total:,.0f} millones USD",
         margin=dict(t=50, l=25, r=25, b=25)
     )
     st.plotly_chart(fig)
+
+
+col1, col2 = st.columns(2, vertical_alignment="bottom", border=True)
+
+with col1:
+    impacto_economico()
+
+with col2:
+    pagos_estado()
+
+col3, col4 = st.columns(2, vertical_alignment="bottom", border=True)
+with col3:
+    empleo_local_importado()
+
+with col4:
+    vee()
+
+col5, col6 = st.columns(2, border=True, vertical_alignment="bottom")
+with col5:
+    valor_irradiado()
+
+with col6:
+    impulso_empleo()
